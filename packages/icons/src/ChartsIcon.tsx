4d16951fc46d3e711a0df2fc5c41059336df2b93
asimashfaq <asimizb4@gmail.com>
2020-08-29 10:26:59 +0500

    improvement(icons): :bento: Create Icons

---
+++ packages/icons/src/ChartsIcon.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ChartsIconSvg from './assets/chart-pie.svg';
+export const ChartsIcon: React.FC = React.memo((props: any) => {
+  return <ChartsIconSvg {...props}/>;
+});
---
+++ packages/icons/src/PageIcon.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import PageIconSvg from './assets/template.svg';
+export const PageIcon: React.FC = React.memo((props: any) => {
+  return <PageIconSvg {...props}/>;
+});
