9335885d64f13e227bf4b1e0b6c9ed1318e574ff
asimashfaq <asimizb4@gmail.com>
2020-08-30 12:45:29 +0500

    feat(backend): :sparkles: Add Subscription Plan resolver for graphql

--- packages/backend/src/subscription_plans/subscription_plan.module.ts
+++ packages/backend/src/subscription_plans/subscription_plan.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-
-import { SubscriptionPlanController } from './service_plan.controller';
+import { SubscriptionPlanController } from './subscription_plan.controller';
+import { SubscriptionPlanResolver } from './subscription_plan.resolver';
 
 @Module({
   controllers: [SubscriptionPlanController],
+  providers: [SubscriptionPlanResolver],
   exports: [],
 })
 export class SubscriptionPlanModule {}
