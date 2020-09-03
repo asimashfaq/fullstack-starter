36a9d69f11058c206dff78bab49d3906b74368aa
asimashfaq <asimizb4@gmail.com>
2020-09-03 17:29:21 +0500

    build(ui): :wrench: Move from babel to tsdx

---
+++ packages/ui/tsdx.config.js
@@ -0,0 +1,24 @@
+const postcss = require('rollup-plugin-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+module.exports = {
+  rollup(config, options) {
+    config.plugins.push(
+      postcss({
+        plugins: [
+          autoprefixer(),
+          cssnano({
+            preset: 'default',
+          }),
+        ],
+        inject: false,
+        modules: true,
+
+        // only write out CSS for the first bundle (avoids pointless extra files):
+        extract: 'style.css',
+      }),
+    );
+    return config;
+  },
+};
